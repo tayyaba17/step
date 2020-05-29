17265d6e23fcb03427068b887872ff4d29cd148f
Tayyaba <tayyabaxali@gmail.com>
2020-05-29 14:02:00 -0500

    Update file to remove unnecessary code and improve function showSlides.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -31,8 +31,7 @@ function addRandomFact() {
   factContainer.innerText = fact;
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 0;
 
 // Next/previous controls
 function plusSlides(n) {
@@ -46,16 +45,11 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  var slides = document.getElementsByClassName("slides-fade");
+  if (n > slides.length) {slideIndex = 0}
+  if (n < 0) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
