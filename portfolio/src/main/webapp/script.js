f17cc4f6b67054c3724d519a2f9595e45a52bb5f
Tayyaba <tayyabaxali@gmail.com>
2020-06-09 19:14:17 +0000

    Script file that contains info for chart implementation.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -78,3 +78,26 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = 'block';
 }
+
+
+/** Creates a chart and adds it to the page. */
+function drawChart() {
+  const data = new google.visualization.DataTable();
+  data.addColumn('string', 'Animal');
+  data.addColumn('number', 'Count');
+        data.addRows([
+          ['Lions', 10],
+          ['Tigers', 5],
+          ['Bears', 15]
+        ]);
+
+  const options = {
+    'title': 'Zoo Animals',
+    'width':500,
+    'height':400
+  };
+
+  const chart = new google.visualization.PieChart(
+      document.getElementById('chart-container'));
+  chart.draw(data, options);
+}
